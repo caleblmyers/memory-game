eb97df3a3e132ec9436b6dc5375fd448164d8b63
Caleb Myers <cmyers5108@gmail.com>
2019-08-30 00:43:32 -0400

    Updates

---
+++ precache-manifest.0fd7e7b411fe3265d2baa76fb336267b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c110ec26a046d0254e7a939e02bdf210",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "20ffde853720561518ff",
+    "url": "/memory-game/static/css/main.ff410d76.chunk.css"
+  },
+  {
+    "revision": "fc69846b7b79f2af2887",
+    "url": "/memory-game/static/js/2.f29c87ce.chunk.js"
+  },
+  {
+    "revision": "20ffde853720561518ff",
+    "url": "/memory-game/static/js/main.eb77fc37.chunk.js"
+  },
+  {
+    "revision": "c019a75bf003c7d0da7f",
+    "url": "/memory-game/static/js/runtime~main.1dba9507.js"
+  }
+]);
